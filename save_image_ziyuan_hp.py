33eb1248e241c43b2d2d496f46fe8cb822e080f1
rs-lsl <66960705+rs-lsl@users.noreply.github.com>
2023-08-07 22:14:58 +0800

    Update and rename save_img_ziyuan_hm.py to save_image_ziyuan_hp.py

--- save_image_ziyuan_hp.py
+++ save_image_ziyuan_hp.py
@@ -1,5 +1,4 @@
-import sys
-
+import sys 
 sys.path.append('/home/aistudio/code')
 sys.path.append('/home/aistudio/code/SSR')
 import numpy as np
@@ -10,14 +9,14 @@ import random
 import time
 import signal
 import cv2
-import paddle
+import torch
+#import paddle.nn.functional as F
 
 # from function import generate_cropdata
 # from function import Crop_traindata_ssr
 from function import blur_downsampling
 from function import Downsampler
 from vca import vca
-
 
 def all_valid(data, axis=0):
     # a, b, c = data.shape
@@ -33,11 +32,9 @@ def all_valid(data, axis=0):
     else:
         return False
 
-
 def read_srf(filename, skiprows=11):
     data = np.loadtxt(filename, skiprows=skiprows)
     return data
-
 
 def read_band(filename):  # 读取wv2影像波段和波宽
 
@@ -47,14 +44,13 @@ def read_band(filename):  # 读取wv2影像波段和波宽
     band = str.split(f)
     band = [float(x) for x in band]
     length = len(np.array(band))
-    band_center = np.array(band)[:int(length / 2)]
-    band_width = np.array(band)[int(length / 2):]
+    band_center = np.array(band)[:int(length/2)]
+    band_width = np.array(band)[int(length/2):]
 
     if band_center[0] > 100:
         return band_center, band_width
     else:
-        return band_center * 1000, band_width * 1000
-
+        return band_center*1000, band_width*1000
 
 def generate_srf(filename4):  # 产生和每个高光谱波段
     srf = read_srf(filename4[0])
@@ -76,8 +72,57 @@ def generate_srf(filename4):  # 产生和每个高光谱波段
 
     return srf_simu
 
+def generate_data(ratio_hs = 12):
 
-def generate_data(ratio=3):
+    patch_size = 64
+    band_num = 126
+    img_num = 2
+    
+    ratio_mss = 4
+    endmembers_num = 5
+    band_mss = 5
+
+    # filename5 = [
+    # '/home/aistudio/work/little_file/envi_plot_wv2.txt',
+    # '/home/aistudio/work/little_file/hyperspectral_band_jiaxing.txt'
+    # ]
+    # srf_simu = np.float32(generate_srf(filename5))
+    # srf_pan = np.expand_dims(np.expand_dims(np.load('/home/aistudio/data/data96268/srf_pan.npy'), axis=-1), axis=-1)
+    # srf_pan = np.expand_dims(np.expand_dims(np.load('/home/aistudio/work/Chikusei/srf_pan_Chikusei.npy'), axis=-1), axis=-1)
+
+    Downsampler12 = Downsampler(12, padding=3)
+
+    hs_path = '/home/aistudio/data/data95831/HS_ziyuan.npy'
+    pan_path = '/home/aistudio/data/data95831/pan_ziyuan.npy'
+    # save_dir = "/home/aistudio/work/ziyuan_hm"
+
+    original_hs = np.float32(np.load(hs_path) / 4095.0)#[:, :1000, :1000]
+    # print(original_hs.shape)
+    original_hs = original_hs[:, :int(original_hs.shape[1]-original_hs.shape[1]%12), :int(original_hs.shape[2]-original_hs.shape[2]%12)]
+ 
+    original_pan = np.float32(np.load(pan_path) / 4095.0)[None, ...]#[:, :1000*12, :1000*12]
+    # print(original_pan.shape)
+    original_pan = original_pan[:, :int(original_pan.shape[1]-original_pan.shape[1]%(12*12)), :int(original_pan.shape[2]-original_pan.shape[2]%(12*12))]
+
+    # np.save(save_dir+'/hs_30m.npy', original_hs)
+    # np.save(save_dir+'/ms_10m.npy', original_pan)
+    print(original_hs.shape)
+    print(original_pan.shape)
+        
+    temp_blur_hs = Downsampler12(torch.tensor(np.expand_dims(original_hs, axis=0)))
+    print(temp_blur_hs.shape)
+    temp_blur_hs = np.squeeze(temp_blur_hs.numpy(), axis=0)
+
+    temp_blur_pan = Downsampler12(torch.tensor(np.expand_dims(original_pan, axis=0)))
+    print(temp_blur_pan.shape)
+    temp_blur_pan = np.squeeze(temp_blur_pan.numpy(), axis=0)
+
+    # np.save(save_dir+'/hs_90m.npy', temp_blur_hs)
+    # np.save(save_dir+'/ms_30m.npy', temp_blur_pan)
+    return temp_blur_hs, temp_blur_pan, original_hs
+
+def generate_data_high():
+
     patch_size = 64
     band_num = 126
     img_num = 2
@@ -94,39 +139,25 @@ def generate_data(ratio=3):
     # srf_pan = np.expand_dims(np.expand_dims(np.load('/home/aistudio/data/data96268/srf_pan.npy'), axis=-1), axis=-1)
     # srf_pan = np.expand_dims(np.expand_dims(np.load('/home/aistudio/work/Chikusei/srf_pan_Chikusei.npy'), axis=-1), axis=-1)
 
-    Downsampler12 = Downsampler(ratio, padding=3)
+    Downsampler12 = Downsampler(12, padding=3)
 
-    hs_path = '/home/aistudio/data/data120965/hs_last.npy'
-    pan_path = '/home/aistudio/data/data120965/ms_last.npy'
+    hs_path = '/home/aistudio/data/data95831/HS_ziyuan.npy'
+    pan_path = '/home/aistudio/data/data95831/pan_ziyuan.npy'
     # save_dir = "/home/aistudio/work/ziyuan_hm"
 
-    original_hs = np.float32(np.load(hs_path) / 4095.0)  # [:, :1000, :1000]
-    original_hs = original_hs[:, :int(original_hs.shape[1] - original_hs.shape[1] % 48),
-                  :int(original_hs.shape[2] - original_hs.shape[2] % 48)]
-
-    original_pan = np.float32(np.load(pan_path) / 4095.0)  # [:, :1000*12, :1000*12]
-    original_pan = original_pan[:, :int(original_pan.shape[1] - original_pan.shape[1] % (48 * 3)),
-                   :int(original_pan.shape[2] - original_pan.shape[2] % (48 * 3))]
-
-    # np.save(save_dir+'/hs_30m.npy', original_hs)
-    # np.save(save_dir+'/ms_10m.npy', original_pan)
-    print(original_hs.shape)
-    print(original_pan.shape)
-
-    temp_blur_hs = Downsampler12(paddle.to_tensor(np.expand_dims(original_hs, axis=0), dtype='float32'))
-    print(temp_blur_hs.shape)
-    temp_blur_hs = np.squeeze(temp_blur_hs.numpy(), axis=0)
-
-    temp_blur_pan = Downsampler12(paddle.to_tensor(np.expand_dims(original_pan, axis=0), dtype='float32'))
-    print(temp_blur_pan.shape)
-    temp_blur_pan = np.squeeze(temp_blur_pan.numpy(), axis=0)
+    original_hs = np.float32(np.load(hs_path) / 4095.0)#[:, :200, :200]
+    # print(original_hs.shape)
+    original_hs = original_hs[:, :int(original_hs.shape[1]-original_hs.shape[1]%12), :int(original_hs.shape[2]-original_hs.shape[2]%12)]
+ 
+    original_pan = np.float32(np.load(pan_path) / 4095.0)[None, ...]#[:, :200*12, :200*12]
+    # print(original_pan.shape)
+    original_pan = original_pan[:, :int(original_pan.shape[1]-original_pan.shape[1]%(12*12)), :int(original_pan.shape[2]-original_pan.shape[2]%(12*12))]
 
     # np.save(save_dir+'/hs_90m.npy', temp_blur_hs)
     # np.save(save_dir+'/ms_30m.npy', temp_blur_pan)
-    return temp_blur_hs, temp_blur_pan, original_hs
+    return original_hs, original_pan
 
-
-def crop_data(hs_data, pan_data, label_data, training_size=64):
+def crop_data(hs_data, pan_data, label_data, training_size=64, step_facter=2):
     train_ratio = 0.8
     train_factor = 1
     test_factor = 1  # 数值越大表明测试影像数量越少
@@ -150,11 +181,11 @@ def crop_data(hs_data, pan_data, label_data, training_size=64):
     # pan_test = np.float32(np.load('/home/aistudio/work/ziyuan_hm/ms_10m.npy'))
 
     train_hs_image, train_pan_image, train_label = \
-        Crop_traindata_three(hs_data,  # 可调整size
-                             pan_data,
-                             label_data,
-                             training_size,
-                             test=False, step_facter=2)
+        Crop_traindata_three(hs_data,             # 可调整size
+                            pan_data,
+                            label_data,
+                            training_size,
+                            test=False, step_facter=step_facter, ratio=int(pan_data.shape[2]/hs_data.shape[2]))
 
     # test_hs_image, test_pan_image = \
     #     Crop_traindata_two(hs_test,
@@ -167,7 +198,6 @@ def crop_data(hs_data, pan_data, label_data, training_size=64):
 
     return train_hs_image, train_pan_image, train_label
 
-
 def Crop_traindata_two(image_ms, image_pan, size, test=False, step_facter=1, ratio=3):
     image_ms_all = []
     image_pan_all = []
@@ -177,10 +207,10 @@ def Crop_traindata_two(image_ms, image_pan, size, test=False, step_facter=1, rat
     print('croping ' + temp_name + ' images...')
     print(image_ms.shape)
 
-    for j in range(0, image_ms.shape[1] - size, int(size / step_facter)):
-        for k in range(0, image_ms.shape[2] - size, int(size / step_facter)):
-            temp_image_ms = image_ms[:, j:j + size, k:k + size]
-            temp_image_pan = image_pan[:, j * ratio:(j + size) * ratio, k * ratio:(k + size) * ratio]
+    for j in range(0, image_ms.shape[1] - size, int(size/step_facter)):
+        for k in range(0, image_ms.shape[2] - size, int(size/step_facter)):
+            temp_image_ms = image_ms[:, j :j + size, k :k + size]
+            temp_image_pan = image_pan[:, j*ratio :(j + size)*ratio, k*ratio :(k + size)*ratio]
 
             if all_valid(temp_image_ms, axis=0):
                 image_ms_all.append(temp_image_ms)
@@ -190,12 +220,11 @@ def Crop_traindata_two(image_ms, image_pan, size, test=False, step_facter=1, rat
     image_pan_all = np.array(image_pan_all, dtype='float32')
 
     print("size of test_lrmsimage:{}".
-          format(image_ms_all.shape))
+            format(image_ms_all.shape))
     print("size of test_hrpanimage:{}".
-          format(image_pan_all.shape))
+            format(image_pan_all.shape))
 
     return image_ms_all, image_pan_all
-
 
 def Crop_traindata_three(image_ms, image_pan, image_label, size, test=False, step_facter=1, ratio=3):
     image_ms_all = []
@@ -207,11 +236,11 @@ def Crop_traindata_three(image_ms, image_pan, image_label, size, test=False, ste
     print('croping ' + temp_name + ' images...')
     print(image_ms.shape)
 
-    for j in range(0, image_ms.shape[1] - size, int(size / step_facter)):
-        for k in range(0, image_ms.shape[2] - size, int(size / step_facter)):
-            temp_image_ms = image_ms[:, j:j + size, k:k + size]
-            temp_image_pan = image_pan[:, j * ratio:(j + size) * ratio, k * ratio:(k + size) * ratio]
-            temp_label = image_label[:, j * ratio:(j + size) * ratio, k * ratio:(k + size) * ratio]
+    for j in range(0, image_ms.shape[1] - size, int(size/step_facter)):
+        for k in range(0, image_ms.shape[2] - size, int(size/step_facter)):
+            temp_image_ms = image_ms[:, j :j + size, k :k + size]
+            temp_image_pan = image_pan[:, j*ratio :(j + size)*ratio, k*ratio :(k + size)*ratio]
+            temp_label = image_label[:, j*ratio :(j + size)*ratio, k*ratio :(k + size)*ratio]
 
             if all_valid(temp_image_ms, axis=0):
                 # print(temp_image_pan.shape)
@@ -225,14 +254,13 @@ def Crop_traindata_three(image_ms, image_pan, image_label, size, test=False, ste
     image_label_all = np.array(label, dtype='float32')
 
     print("size of lrmsimage:{}".
-          format(image_ms_all.shape))
+            format(image_ms_all.shape))
     print("size of hrpanimage:{}".
-          format(image_pan_all.shape))
+            format(image_pan_all.shape))
     print("size of labelimage:{}".
-          format(image_label_all.shape))
+            format(image_label_all.shape))
 
     return image_ms_all, image_pan_all, image_label_all
-
 
 def unsupervise_crop():
     img_num = 1
@@ -246,7 +274,7 @@ def unsupervise_crop():
     ratio = 16
 
     region = 'jiaxing'
-    base_dir1 = '/home/aistudio/work/' + region + '/Line'
+    base_dir1 = '/home/aistudio/work/'+region+'/Line'
     base_dir0 = '/home/aistudio/data/data96268/Line'
 
     # 多张影像
@@ -256,11 +284,10 @@ def unsupervise_crop():
     label_data = []
     # abun16 = []
     for i2 in range(img_num):
+ 
         hs_data.append(np.float32(np.load('/home/aistudio/data/data95831/HS_ziyuan.npy')[:-2, :1000, :1000]) / 4095.0)
-
-        pan_data.append(np.expand_dims(
-            np.float32(np.load('/home/aistudio/data/data95831/pan_ziyuan.npy')[:1000 * 12, :1000 * 12]) / 4095.0,
-            axis=0))
+ 
+        pan_data.append(np.expand_dims(np.float32(np.load('/home/aistudio/data/data95831/pan_ziyuan.npy')[:1000*12, :1000*12]) / 4095.0, axis=0))
 
     # 安全
     assert len(hs_data) == img_num
@@ -284,15 +311,15 @@ def unsupervise_crop():
 
     np.save('/home/aistudio/work/ziyuan/train_hs.npy', result_hs)
     np.save('/home/aistudio/work/ziyuan/train_pan.npy', result_pan)
-
+    
 
 def generate_patch_ms(train_num=1000, ms_band=4):
     hs_data = np.load('/home/aistudio/work/jiaxing/label_hs.npy')
     test_data = hs_data[1000:, :, :, :]
 
     filename5 = [
-        '/home/aistudio/work/little_file/envi_plot_wv2.txt',
-        '/home/aistudio/work/little_file/hyperspectral_band_jiaxing.txt'
+    '/home/aistudio/work/little_file/envi_plot_wv2.txt',
+    '/home/aistudio/work/little_file/hyperspectral_band_jiaxing.txt'
     ]
     srf_simu = np.float32(generate_srf(filename5))
     print(srf_simu.shape)
